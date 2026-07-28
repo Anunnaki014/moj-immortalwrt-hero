61e13a6617fe24a76423b97089e822c5357583ce
Anunnaki014 <anunnaki014play@gmail.com>
2026-07-28 23:34:55 +0300

    Update mt7981.mk

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -815,6 +815,7 @@ define Device/keenetic_kn-1012
   DEVICE_MODEL := Hero/Giga (KN-1012)
   DEVICE_VARIANT := NAND
   DEVICE_DTS := mt7981-keenetic-kn-1012
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
   DEVICE_COMPATIBLE := keenetic,kn-1012 mediatek,mt7981
   IMAGE_SIZE := 114688k
   SUPPORTED_DEVICES := keenetic,kn-1012
