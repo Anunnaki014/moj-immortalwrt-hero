fb7ce3c77519997bc67ec69a5c80787fa0a12f67
Anunnaki014 <anunnaki014play@gmail.com>
2026-08-07 08:05:29 +0300

    Update mt7981.mk

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -819,7 +819,7 @@ define Device/keenetic_kn-1012
   DEVICE_COMPATIBLE := keenetic,kn-1012 mediatek,mt7981
   IMAGE_SIZE := 114688k
   SUPPORTED_DEVICES := keenetic,kn-1012
-  DEVICE_PACKAGES := kmod-usb3 kmod-mt7981-firmware wpad-basic-wolfssl
+  DEVICE_PACKAGES := kmod-usb3 kmod-mt7981-firmware wpad-basic-wolfssl luci uhttpd uhttpd-mod-ubus
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
   KERNEL_INITRAMFS := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
 endef
