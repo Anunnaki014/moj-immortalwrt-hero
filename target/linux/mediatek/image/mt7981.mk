b997f7283824c822c0a04973e2ada16d027755c8
Anunnaki014 <anunnaki014play@gmail.com>
2026-08-13 15:04:05 +0300

    Update mt7981.mk

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -810,16 +810,18 @@ define Device/routerich_ax3000
 endef
 TARGET_DEVICES += routerich_ax3000
 
-define Device/keenetic_kn-1012
+define Device/keenetic,kn-1012
   DEVICE_VENDOR := Keenetic
   DEVICE_MODEL := Hero/Giga (KN-1012)
   DEVICE_VARIANT := NAND
   DEVICE_DTS := mt7981-keenetic-kn-1012
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  DEVICE_COMPATIBLE := keenetic,kn-1012,mediatek,mt7981
+  DEVICE_COMPATIBLE := keenetic,kn-1012, mediatek,mt7981
   IMAGE_SIZE := 114688k
+  SUPPORTED_DEVICES := keenetic,kn-1012
   DEVICE_PACKAGES := kmod-usb3 kmod-mt7981-firmware wpad-basic-wolfssl luci uhttpd uhttpd-mod-ubus
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/sysupgrade.bin := sysupgrade-tar
   KERNEL_INITRAMFS := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
 endef
-TARGET_DEVICES += keenetic_kn-1012
+TARGET_DEVICES += keenetic,kn-1012
+
